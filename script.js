c3a0152272baf837dd2a566623ca94b97eadf43a
dizzysnazzy <dismaskipchumba2@gmail.com>
2024-02-23 02:02:38 +0300

    Updated

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-fetch("./templates/header.html")
-    .then(response => response.text())
-    .then(data => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, "text/html");
-        document.getElementById("header-placeholder").innerHTML = doc.querySelector("header").innerHTML;
-        // document.getElementById("footer-placeholder").innerHTML = doc.querySelector("footer").innerHTML;
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("./templates/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, "text/html");
-            document.getElementById("footer-placeholder").innerHTML = doc.querySelector("footer").innerHTML;
-    });
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
